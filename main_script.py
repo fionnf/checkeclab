ed3bc259c2edf289bf43d2f08e5af6374abea613
F.M. Eckardt Ferreira <f.m.eckardt.ferreira@student.rug.nl>
2024-02-21 08:54:15 +0100

    Using basic galvani

--- main_script.py
+++ main_script.py
@@ -1,7 +1,6 @@
-import eclabfiles as ecf
+from galvani import BioLogic
+import pandas as pd
 
-mpr_file = r"C:\Users\fionn\Desktop\FF054BioLgic_b_C06.mpr"
-
-df = ecf.to_df(mpr_file)
-
+mpr_file = BioLogic.MPRfile('FF054BioLgic_C06.mpr')
+df = pd.DataFrame(mpr_file.data)
 print(df)
